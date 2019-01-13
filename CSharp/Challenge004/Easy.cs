d5498013724ff8f98c523a8d2ee48f6929d2e357
Kalen Rose <kalenrose@outlook.com>
2019-01-13 00:14:05 -0500

    Add solution for Challenge 004 (easy) in c#

---
+++ CSharp/Challenge004/Easy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenge004{
+    class Easy{
+        public static void Run() {
+            char[] alphaNumbericSymbols = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!@#$%^&*()-_!@#$%^&*()-_!@#$%^&*()".ToCharArray();
+            Random r = new Random();
+            string password = "";
+
+            for(int i = 0; i < 15; i++) 
+                password += alphaNumbericSymbols[r.Next(0, alphaNumbericSymbols.Length)];
+
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ CSharp/Challenge004/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Challenge004
+{
+    class Program {
+        static void Main(string[] args) {
+            do {
+                Console.Write("\nType 1 for easy, 2 for intermediate, 3 for hard: ");
+
+                switch (Console.ReadLine()) {
+                    case "1":
+                    Easy.Run();
+                    break;
+                    case "2":
+                    //Intermediate.Run();
+                    break;
+                    case "3":
+                    //Hard.Run();
+                    break;
+                }
+
+            } while (true);
+        }
+    }
+}
