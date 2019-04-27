7143c221a19c884061bb1c24be496206b83a91c0
Kalen Rose <kalenrose@outlook.com>
2019-04-27 17:01:41 -0400

    Add solution for challenge 006 (easy) with C#

---
+++ CSharp/Challenge006/Easy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Challenge006 {
+	internal class Easy {
+		static bool add = false;
+
+		internal static void Run() {
+			Console.WriteLine(Math.Round(CalcPi(),15));
+		}
+
+		internal static double CalcPi() {
+			double num = 1.0;
+
+			for(double i = 3; i < 10_000_000_000; i += 2) {
+				if (add) {
+					num += (1.0 / i);
+					add = false;
+				} else {
+					num -= (1.0 / i);
+					add = true;
+				}
+			}
+
+			return num * 4;
+		}
+	}
+}
---
+++ CSharp/Challenge006/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge006 {
+	class Program {
+		static void Main(string[] args) {
+			do {
+				Console.Write("\nType 1 for easy, 2 for intermediate, 3 for hard: ");
+
+				switch (Console.ReadLine()) {
+				case "1":
+					Console.WriteLine("Calculate pi accurately to at least 30 decimal places.\nDue to limitations of C#, only up to 15 digits");
+					Easy.Run();
+					break;
+				case "2":
+					//Intermediate.Run();
+					break;
+				case "3":
+					//Hard.Run();
+					break;
+				}
+
+			} while (true);
+		}
+	}
+}
